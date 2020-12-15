d4cd1b58872bab212a6746b940e927057d4591c9
fractalwrench <fractalwrench@gmail.com>
2020-12-15 10:03:07 +0000

    setup example app to not use bugsnag

--- examples/sdk-app-example/src/main/java/com/foo/android/example/ExampleApplication.kt
+++ examples/sdk-app-example/src/main/java/com/foo/android/example/ExampleApplication.kt
@@ -1,9 +1,9 @@
 package com.foo.android.example
 
 import android.app.Application
-import com.bugsnag.android.Bugsnag
-import com.bugsnag.android.Configuration
-import com.bugsnag.android.ErrorTypes
+//import com.bugsnag.android.Bugsnag
+//import com.bugsnag.android.Configuration
+//import com.bugsnag.android.ErrorTypes
 
 class ExampleApplication : Application() {
 
@@ -25,10 +25,10 @@ class ExampleApplication : Application() {
     override fun onCreate() {
         super.onCreate()
 
-        val config = Configuration.load(this)
-        config.setUser("123456", "joebloggs@example.com", "Joe Bloggs")
-        config.addMetadata("user", "age", 31)
-        Bugsnag.start(this, config)
+//        val config = Configuration.load(this)
+//        config.setUser("123456", "joebloggs@example.com", "Joe Bloggs")
+//        config.addMetadata("user", "age", 31)
+//        Bugsnag.start(this, config)
 
         // Initialise native callbacks
 //        performNativeBugsnagSetup()
